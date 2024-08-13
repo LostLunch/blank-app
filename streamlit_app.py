e790d6257d6ac1ed0532a2d755085393b521b412
LostLunch <167606075+LostLunch@users.noreply.github.com>
2024-08-13 22:34:53 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from PIL import Image
 st.set_page_config(layout="wide")
 
 # 로고 이미지를 열고, 크기를 조절합니다.
-logo = Image.open("logo.png")
+#logo = Image.open("logo.png")
 
 #레이아웃 설정
 col1, col2, col3, col4 = st.columns([1,1,1,1])
@@ -17,19 +17,19 @@ with col1:
     st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
     st.write('a1234'+"일차")#a1234에 몇일차인지 입력
     st.write("장소")
-    st.image('장소이미지')
+    #st.image('장소이미지')
 with col2:
     st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
     st.write('a1234'+"일차")#a1234에 몇일차인지 입력
     st.write("장소")
-    st.image('장소이미지')
+    #st.image('장소이미지')
 with col3:
     st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
     st.write('a1234'+"일차")#a1234에 몇일차인지 입력
     st.write("장소")
-    st.image('장소이미지')
+    #st.image('장소이미지')
 with col4:
     st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
     st.write('a1234'+"일차")#a1234에 몇일차인지 입력
     st.write("장소")
-    st.image('장소이미지')
+    #st.image('장소이미지')
