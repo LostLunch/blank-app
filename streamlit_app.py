b6ea4bf45cb3d37a08180458d83af0ea554bc06a
LostLunch <167606075+LostLunch@users.noreply.github.com>
2024-08-13 16:07:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,27 +2,23 @@ import streamlit as st
 
 # CSS를 사용하여 슬라이드쇼 설정
 st.markdown("""
-    <style>
-    body {
-        margin: 0;
-        padding: 0;
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-size: cover;
-        background-position: center;
-        animation: slide 15s infinite;
-    }
-
-    @keyframes slide {
-        0% { background-image: url('https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80'); }
-        33% { background-image: url('https://images.unsplash.com/photo-1610016302534-6f67f1c968d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1075&q=80'); }
-        66% { background-image: url('https://images.unsplash.com/photo-1516550893923-42d28e5677af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=872&q=80'); }
-        100% { background-image: url('https://images.unsplash.com/photo-1541343672885-9be56236302a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'); }
-    }
-    </style>
-    """, unsafe_allow_html=True)
-
-st.title("Welcome to My Travel Planner!")
-st.write("Create your perfect travel itinerary in just a few clicks.")
+<!DOCTYPE html>
+<html>
+    <head>
+    	<meta charset="utf-8"/>
+    	<title>Kakao 지도 시작하기</title>
+    </head>
+    <body>
+    	<div id="map" style="width:500px;height:400px;"></div>
+    	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=aa4919bf06af8e8a36733ddb85e52ff3"></script>
+    	<script>
+    		var container = document.getElementById('map');
+    		var options = {
+    			center: new kakao.maps.LatLng(33.450701, 126.570667),
+    			level: 3
+    		};
+    
+    		var map = new kakao.maps.Map(container, options);
+    	</script>
+    </body>
+</html>""",)
