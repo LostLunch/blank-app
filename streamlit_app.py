52bb37560d94de046889b31ee6f563d043e1680f
LostLunch <167606075+LostLunch@users.noreply.github.com>
2024-08-13 22:34:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,35 @@
+#작은 따옴표는 변수임
 import streamlit as st
+from PIL import Image
+# 페이지 레이아웃 설정
+st.set_page_config(layout="wide")
 
-# CSS를 사용하여 슬라이드쇼 설정
-st.markdown("""
-<!DOCTYPE html>
-<html>
-    <head>
-    	<meta charset="utf-8"/>
-    	<title>Kakao 지도 시작하기</title>
-    </head>
-    <body>
-    	<div id="map" style="width:500px;height:400px;"></div>
-    	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=aa4919bf06af8e8a36733ddb85e52ff3"></script>
-    	<script>
-    		var container = document.getElementById('map');
-    		var options = {
-    			center: new kakao.maps.LatLng(33.450701, 126.570667),
-    			level: 3
-    		};
-    
-    		var map = new kakao.maps.Map(container, options);
-    	</script>
-    </body>
-</html>""",unsafe_allow_html=True)
-st.write("집가고싶다")
+# 로고 이미지를 열고, 크기를 조절합니다.
+logo = Image.open("logo.png")
+
+#레이아웃 설정
+col1, col2, col3, col4 = st.columns([1,1,1,1])
+
+with col3,col4:
+    print('설명')#설명
+
+with col1:
+    st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
+    st.write('a1234'+"일차")#a1234에 몇일차인지 입력
+    st.write("장소")
+    st.image('장소이미지')
+with col2:
+    st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
+    st.write('a1234'+"일차")#a1234에 몇일차인지 입력
+    st.write("장소")
+    st.image('장소이미지')
+with col3:
+    st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
+    st.write('a1234'+"일차")#a1234에 몇일차인지 입력
+    st.write("장소")
+    st.image('장소이미지')
+with col4:
+    st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
+    st.write('a1234'+"일차")#a1234에 몇일차인지 입력
+    st.write("장소")
+    st.image('장소이미지')
