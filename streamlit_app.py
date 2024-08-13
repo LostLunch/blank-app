9dd66c476877b99db0a7cbdc940922ea3e905edd
LostLunch <167606075+LostLunch@users.noreply.github.com>
2024-08-14 01:09:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,31 +5,46 @@ from PIL import Image
 st.set_page_config(layout="wide")
 
 # 로고 이미지를 열고, 크기를 조절합니다.
-#logo = Image.open("logo.png")
+logo = Image.open("logo.png")
 
-#레이아웃 설정
-col1, col2, col3, col4 = st.columns([1,1,1,1])
+# 헤더 섹션을 만들기 위해 열(column)을 나눕니다.
+header_col1, header_col2, header_col3 = st.columns([1,1,4])
 
-with col3,col4:
-    print('설명')#설명
 
-with col1:
-    st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
-    st.write('a1234'+"일차")#a1234에 몇일차인지 입력
-    st.write("장소")
-    #st.image('장소이미지')
-with col2:
-    st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
-    st.write('a1234'+"일차")#a1234에 몇일차인지 입력
-    st.write("장소")
-    #st.image('장소이미지')
-with col3:
-    st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
-    st.write('a1234'+"일차")#a1234에 몇일차인지 입력
-    st.write("장소")
-    #st.image('장소이미지')
-with col4:
-    st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
-    st.write('a1234'+"일차")#a1234에 몇일차인지 입력
-    st.write("장소")
-    #st.image('장소이미지')
+
+with header_col1:
+    # 첫 번째 열에 로고 이미지를 추가합니다.
+    st.image(logo, use_column_width=True)
+
+with header_col2:
+    abc = 'A'
+    st.markdown(f"<p style='font-size:18px;'>코스 {abc}</p>", unsafe_allow_html=True)#abc에 "A","B","C"중 하나 선택
+
+with header_col3:
+    with st.expander("설명"):
+        st.write('''
+        설명
+        ''')
+        #st.image('필요할 시 이미지 넣고 필요 없으면 지우기')
+def day():
+    #레이아웃 설정
+    col4, col5, col6 = st.columns([1,1,1])
+
+    with col4:
+        st.write('a1234'+"일차")#a1234에 몇일차인지 입력
+        st.write('장소')
+        #st.image('장소이미지')
+        st.write('장소 설명')
+    with col5:
+        st.write('a1234'+"일차")#a1234에 몇일차인지 입력
+        st.write('장소')
+        #st.image('장소이미지')
+        st.write('장소 설명')
+    with col6:
+        st.container():
+            st.write('a1234'+"일차")#a1234에 몇일차인지 입력
+            st.write('장소')
+            #st.image('장소이미지')
+            st.write('장소 설명')
+for i in range(3):
+    day()
